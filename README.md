dbc566a4c7fa276fb5cbfab9868cef7ad14c5679
Jack Wu <jackwu8203@gmail.com>
2023-06-01 00:11:35 +0800

    The initial code and complete the basic functions

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
-# gptspeaker
-The GptSpeaker uses a Raspberry Pi (or desktop) to enable spoken conversation with OpenAI large language models. This implementation listens to speech, processes the conversation through the OpenAI service, and responds back.
+# GptSpeaker
+* The GptSpeaker uses a Raspberry Pi (or desktop) to enable spoken conversation with OpenAI large language models. This implementation listens to speech, processes the conversation through the OpenAI service, and responds back.
+* This project is written in python which supports Linux/Raspbian, macOS, and Windows.
+* Hardware
+   - $ for [Raspberry PI 3/3B/4/4B](https://aka.ms/maker/rpi/four)
+   - $ for [USB Micro Phone](https://item.taobao.com/item.htm?spm=a230r.1.14.23.315b64e0hmblId&id=668895270969&ns=1&abbucket=1#detail)
+   - $ for Aux Speaker
+   - $ for an SD card (>= 8GB ) (to setup the Raspberry Pi OS)
+* Software
+  - [Azure Cognitive Speech Services](https://aka.ms/friendbot/azurecog)
+    - **Free tier**: 5 audio hours per month and 1 concurrent request. 
+    - **Free $200 credit**: With a new Azure account that can be used during the first 30 days.
+  - [OpenAI](https://aka.ms/maker/openai/pricing)
+    - **$0.002 / 1K tokens / ~750 words**: ChatGPT (gpt-3.5-turbo)
+    - **Free $18 credit**: With a new OpenAI account that can be used during your first 90 days.
+# Setup
+* You will need an instance of Azure Cognitive Services and an OpenAI account. You can run the software on nearly any platform, but let's start with a Raspberry Pi.
+## Raspberry Pi
+* If you are new to Raspberry Pis, check out this [getting started](https://aka.ms/maker/rpi/gettingstarted) guide.
+### 1. OS
+1. Insert an SD card into your PC.
+1. Go to https://aka.ms/maker/rpi/software then download and run the Raspberry Pi Imager. 
+1. Click `Choose OS` and select the Ubuntu 22.04.2 LTS (64-bit).
+1. Click `Choose Storage`, select the SD card.
+1. Click `Write` and wait for the imaging to complete.
+1. Put the SD card into your Raspberry Pi and connect a keyboard, mouse, and monitor.
+1. Complete the initial setup, making sure to configure Wi-Fi.
+### 2. USB Speaker/Microphone
+1. Plug in the USB speaker/microphone if you have not already.
+1. On the Raspberry PI OS desktop, right-click on the volume icon in the top-right of the screen and make sure the USB device is selected.
+1. Right-click on the microphone icon in the top-right of the screen and make sure the USB device is selected.
+## Azure
+The conversational speaker uses Azure Cognitive Service for speech-to-text and text-to-speech. Below are the steps to create an Azure account and an instance of Azure Cognitive Services.
+### 1. Azure Account
+  1. In a web browser, navigate to https://aka.ms/friendbot/azure and click on `Try Azure for Free`.
+  1. Click on `Start Free` to start creating a free Azure account.
+  1. Sign in with your Microsoft or GitHub account.
+  1. After signing in, you will be prompted to enter some information.
+        > NOTE: Even though this is a free account, Azure still requires credit card information. You will not be charged unless you change settings later.
+  1. After your account setup is complete, navigate to https://aka.ms/friendbot/azureportal.
+
+### 2. Azure Cognitive Services
+  1. Sign into your account at https://aka.ms/friendbot/azureportal.
+  1. In the search bar at the top, enter `Cognitive Services`. Under `Marketplace` select `Cognitive Services`. (It may take a few seconds to populate.)
+  1. Verify the correct subscription is selected. Under `Resource Group` select `Create New`. Enter a resource group name (e.g. `conv-speak-rg`).
+  1. Select a region and a name for your instance of Azure Cognitive Services (e.g. `my-conv-speak-cog-001`). 
+        > NOTE: EastUS, WestEurope, or SoutheastAsia are recommended, as those regions tend to support the greatest number of features.  
+  1. Click on `Review + Create`. After validation passes, click `Create`.
+  1. When deployment has completed you can click `Go to resource` to view your Azure Cognitive Services resource.
+  1. On the left side navigation bar, under `Resourse Management`, select `Keys and Endpoint`.
+  1. Copy either of the two Cognitive Services keys. Save this key in a secure location for later.
+
+  > Windows 11 users: If the application is stalling when calling the text-to-speech API, make sure you have applied all current security updates ([link](https://learn.microsoft.com/en-us/windows/release-health/resolved-issues-windows-11-22h2#2924msgdesc)).
+
+## OpenAI
+The conversational speaker uses OpenAI's models to hold a friendly conversation. Below are the steps to create a new account and access the AI models.
+### 1. OpenAI Account
+  1. In a web browser, navigate to https://aka.ms/maker/openai. Click `Sign up`.
+        > NOTE: can use a Google account, Microsoft account, or email to create a new account.
+  1. Complete the sign-up process (e.g., create a password, verify your email, etc.).
+        > NOTE: If you are new to OpenAI, please review the usage guidelines (https://beta.openai.com/docs/usage-guidelines).
+  1. In the top-right corner click on your account. Click on `View API keys`.
+  1. Click `+ Create new secret key`. Copy the generated key and save it in a secure location for later.
+
+  _If you are curious to play with the large language models directly, check out the https://platform.openai.com/playground?mode=chat at the top of the page after logging in to https://aka.ms/maker/openai._
+
+# The Code
+## 1. Code Configuration
+1. The Python Speech SDK package is available for Windows (x64 and x86), Mac x64 (macOS X version 10.14 or later), Mac arm64 (macOS version 11.0 or later), and Linux
+1. On the Raspberry Pi or your PC, open a command-line terminal.
+1. On Ubuntu or Debian, run the following commands for the installation of required packages:
+    ```sh
+    sudo apt-get update
+    sudo apt-get install libssl-dev libasound2
+    ```
+1. On **Ubuntu 22.04 LTS** it is also required to download and install the latest **libssl1.1** package e.g. from http://security.ubuntu.com/ubuntu/pool/main/o/openssl/.
+1. Clone the repo.
+   ```bash
+   git clone https://github.com/jackwuwei/gptspeaker.git
+   ```
+1. Set your API keys: Replace config.json `{AzureCognitiveServices.Key}`and `{AzureCognitiveServices.Region}` with your OpenAI API key and `{OpenAI.Key}` with your OpenAI API key.
+    ```json
+    {
+         "AzureCognitiveServices": {
+            "Key": "AzureCognitiveServicesKey", 
+            "Region": "AzureCognitiveServicesRegion",
+        },
+
+        "OpenAI": {
+            "Key": "OpenAIKey", 
+        }
+    }
+    ```
+1. Run the code!
+   ```bash
+   pip3 install azure-cognitiveservices-speech openai tiktoken
+   python3 gptspeaker.py
+   ```
+## 2. (Optional) Create a custom wake phrase
+The code base has a default wake phrase (`"杰克同学"`) already, which I suggest you use first. If you want to create your own (free!) custom wake word, then follow the steps below.
+  1. Create a custom keyword model using the directions here: https://aka.ms/hackster/microsoft/wakeword. 
+  1. Download the model, extract the `.table` file and copy it to source root directory.
+  1. Update `config.json` file to include your wake phrase file in the build.
+     ```json
+     "AzureCognitiveServices": {
+        "WakePhraseModel": "xxx.table",
+        "WakeWord": "xxx",
+     }
+     ```
+  1. Rebuild and run the project to use your custom wake word.
---
+++ gptspeaker.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jack Wu. All rights reserved.
+# Licensed under the BSD license. See LICENSE.md file in the project root for full license information.
+"""
+Smart Speaker using Azure Speech SDK and OpenAI ChatGPT API
+"""
+import os
+import azure.cognitiveservices.speech as speechsdk
+import openai
+import asyncio
+import json
+from collections import namedtuple
+import tiktoken
+
+EOF = object()
+
+# Load config.json
+def load_config():
+    try:
+        with open('config.json') as f:
+            config = json.load(f, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+            if not config.AzureCognitiveServices.Key or not config.AzureCognitiveServices.Region or not config.OpenAI.Key:
+                raise ValueError("Missing required configuration.")
+            return config
+    except FileNotFoundError:
+        print("Error: config file not found.")
+    except Exception as e:
+        print(f"Error loading config: {e}")
+
+# If tokens greater than 4096, then remove history message
+def truncate_conversation(conversation, max_tokens):
+    total_tokens = 0
+    truncated_conversation = []
+    encoding = tiktoken.get_encoding("cl100k_base")
+
+    for message in reversed(conversation):
+        message_tokens = len(encoding.encode(message['content']))
+        if total_tokens + message_tokens > max_tokens - 100: 
+            print(f'Total tokens is limit {total_tokens + message_tokens}')
+            break
+        total_tokens += message_tokens
+        truncated_conversation.append(message)
+
+    conversation = list(reversed(truncated_conversation))
+
+# Prompts OpenAI with a request and async send sentences to queue.
+async def ask_openai_async(model, prompt, max_token, conversation, queue, ending):
+    # Append user questions
+    conversation.append({"role":"user","content":prompt}) 
+
+    # Count token limit and remove early histroy conversation 
+    truncate_conversation(conversation, max_token)
+    print(conversation)
+    
+    # Save one sentence
+    collected_messages = ""
+
+    # Save whole GPT answer
+    full_answer = ""
+
+    # Ask OpenAI
+    response = await openai.ChatCompletion.acreate(model=model, 
+                                                   messages=conversation,
+                                                   stream=True)
+    
+    # iterate through the stream of events
+    async for chunk in response:
+        chunk_message = chunk['choices'][0]['delta'].get('content', '').replace('\n', ' ').strip()  # extract the message
+        if not chunk_message:
+            continue
+
+        collected_messages += chunk_message  # save the message
+        if collected_messages.endswith(ending): # One sentence
+            print(f"ChatGPT Message received: {collected_messages}")
+            await queue.put(collected_messages)
+            full_answer += collected_messages
+            collected_messages = ""
+
+    # Save history message for continuous conversations
+    conversation.append({"role":"assistant","content":full_answer})
+
+# async read message from queue and synthesized speech
+async def text_to_speech_async(speech_synthesizer, queue):
+    while True:
+        text = await queue.get()
+        if text is EOF:
+            break
+
+        # Azure text to speech output
+        speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+
+        # Check result
+        if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+            print("Speech synthesized to speaker for text [{}]".format(text))
+        elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = speech_synthesis_result.cancellation_details
+            print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+            if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                print("Error details: {}".format(cancellation_details.error_details))
+
+# Detect keyword and wakeup
+def detect_keyword(model, keyword):
+    # Create a local keyword recognizer with the default microphone device for input.
+    keyword_recognizer = speechsdk.KeywordRecognizer()
+
+    done = False
+
+    def recognized_cb(evt):
+        # Only a keyword phrase is recognized. The result cannot be 'NoMatch'
+        # and there is no timeout. The recognizer runs until a keyword phrase
+        # is detected or recognition is canceled (by stop_recognition_async()
+        # or due to the end of an input file or stream).
+        result = evt.result
+        if result.reason == speechsdk.ResultReason.RecognizedKeyword:
+            print("RECOGNIZED KEYWORD: {}".format(result.text))
+        nonlocal done
+        done = True
+
+    def canceled_cb(evt):
+        result = evt.result
+        if result.reason == speechsdk.ResultReason.Canceled:
+            print('CANCELED: {}'.format(result.cancellation_details.reason))
+        nonlocal done
+        done = True
+
+    # Connect callbacks to the events fired by the keyword recognizer.
+    keyword_recognizer.recognized.connect(recognized_cb)
+    keyword_recognizer.canceled.connect(canceled_cb)
+
+    # Start keyword recognition.
+    result_future = keyword_recognizer.recognize_once_async(model)
+    print('Say something starting with "{}" followed by whatever you want...'.format(keyword))
+    result = result_future.get()
+
+    # Read result audio (incl. the keyword).
+    return result.reason == speechsdk.ResultReason.RecognizedKeyword
+
+# Continuously listens for speech input to recognize and send as text to Azure OpenAI
+async def chat_with_open_ai():
+    # Load config.json
+    config = load_config()
+
+    # OpenAI API Key
+    openai.api_key = config.OpenAI.Key
+    gpt_model = config.OpenAI.Model
+
+    # This example requires config.json
+    speech_config = speechsdk.SpeechConfig(subscription=config.AzureCognitiveServices.Key, 
+                                           region=config.AzureCognitiveServices.Region)
+    audio_output_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+
+    # Should be the locale for the speaker's language.
+    speech_config.speech_recognition_language = config.AzureCognitiveServices.SpeechRecognitionLanguage
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    ending_punctuations = (".", "?", "!", ";")
+    if (speech_config.speech_recognition_language == "zh-CN"):
+        ending_punctuations = ("。", "？", "！", "；")
+
+    # The language of the voice that responds on behalf of Azure OpenAI.
+    speech_config.speech_synthesis_voice_name = config.AzureCognitiveServices.SpeechSynthesisVoiceName
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_output_config)
+
+    # The phrase your keyword recognition model triggers on.
+    kws_model = speechsdk.KeywordRecognitionModel(config.AzureCognitiveServices.WakePhraseModel)
+    conversation = [{"role": "system", "content": config.General.SystemPrompt}]
+
+    while True:
+        print("OpenAI is listening. Say '{}' to start.".format(config.AzureCognitiveServices.WakeWord))
+        try:
+            # Detect keyword
+            if (not detect_keyword(kws_model, config.AzureCognitiveServices.WakeWord)):
+                continue
+
+            # Get audio from the microphone and then send it to the TTS service.
+            speech_recognition_result = speech_recognizer.recognize_once_async().get()
+
+            # If speech is recognized, send it to OpenAI and listen for the response.
+            if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
+                if speech_recognition_result.text == config.AzureCognitiveServices.StopWord: 
+                    print("Conversation ended.")
+                    break
+
+                print("Recognized speech: {}".format(speech_recognition_result.text))
+
+                # Create queue for save GPT messages
+                queue = asyncio.Queue()
+
+                # Create async task for ask openai
+                task_ask_gpt = asyncio.create_task(ask_openai_async(gpt_model, 
+                                                                    speech_recognition_result.text, 
+                                                                    config.OpenAI.MaxTokens, 
+                                                                    conversation, 
+                                                                    queue,
+                                                                    ending_punctuations))
+
+                # Add task done callback, add a EOF message to end
+                task_ask_gpt.add_done_callback(lambda _: queue.put_nowait(EOF))
+
+                # Create async task for Text-to-Speech
+                task_ask_tts = asyncio.create_task(text_to_speech_async(speech_synthesizer, queue))
+
+                # Wait all task completed
+                await asyncio.gather(task_ask_gpt, task_ask_tts)
+            elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+                print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
+            elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
+                cancellation_details = speech_recognition_result.cancellation_details
+                print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+                if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                    print("Error details: {}".format(cancellation_details.error_details))
+        except EOFError:
+            continue
+
+if __name__ == '__main__':
+    # Main
+    try:
+        asyncio.run(chat_with_open_ai())
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
